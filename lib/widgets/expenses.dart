9190b72f6727c94d1a607205a500ecd05d111169
raneem-yad <eng.raneem.yad@gmail.com>
2024-08-17 15:39:21 +0200

    Add Dismissible list

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -43,6 +43,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense){
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +61,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(expenses: _registeredExpenses,onRemove: _removeExpense,),
           ),
         ],
       ),
