5d9ab41ba961b11cd2b35a850571bbb10372385e
raneem-yad <eng.raneem.yad@gmail.com>
2024-08-17 14:09:20 +0200

    add a date picker with an expand element

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -13,6 +13,11 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
 
+  void _presentDatePicker(){
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year-1, now.month, now.day);
+    showDatePicker(context: context, firstDate: firstDate, lastDate: now);
+  }
   @override
   void dispose() {
     _titleController.dispose();
@@ -33,13 +38,27 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: '\$ ',
-              label: Text("Amount")
-            ),
+          Row(
+
+            children: [
+              Expanded(child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text("Amount")
+                ),
+              ),),
+              const SizedBox(width: 10,),
+              Expanded(child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Select Date"),
+                  IconButton(onPressed: _presentDatePicker, icon: Icon(Icons.calendar_month))
+                ],
+              ),)
+            ],
           ),
           Row(
             children: [
@@ -50,7 +69,9 @@ class _NewExpenseState extends State<NewExpense> {
                 },
                 child: const Text('Save Expense'),
               ),
-              ElevatedButton(onPressed: (){}, child: const Text("Cancel"))
+              ElevatedButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: const Text("Cancel"))
             ],
           ),
         ],
